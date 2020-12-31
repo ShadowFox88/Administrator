a751f4319f527708686120a09b1fa3aa519590bc
cyrus01337 <cyrus21337@gmail.com>
2020-12-31 03:27:51 +0000

    Add due credit, optimise intelligent command processing

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+from typing import Dict
+
 import discord
 from discord.ext import commands
 
@@ -5,7 +8,9 @@ from base import custom
 
 
 class Administrator(custom.Bot):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        _edit_cache_maximum = kwargs.pop("max_messages", 1000)
+
         super().__init__(
             command_prefix=commands.when_mentioned_or(">>>"),
             activity=discord.Activity(
@@ -15,17 +20,27 @@ class Administrator(custom.Bot):
             allowed_mentions=discord.AllowedMentions(
                 everyone=False,
                 roles=False
-            )
+            ),
+            max_messages=0
         )
-        self._edit_cache = {}
+        self._edit_cache_maximum = _edit_cache_maximum
+        self._edit_cache: Dict[int, discord.Message] = OrderedDict()
         self.home_id = 464446709146320897
 
-        exclude = ["base.cogs.error_handler"]  # noqa
+        exclude = ["base.cogs.error_handler"]  # noqa: F841
         self.load_extensions("./cogs")
 
     @property
     def home(self):
         return self.get_guild(self.home_id)
+
+    def _get_edit_cached_message(self, message_id: int):
+        message_found = self._edit_cache.get(message_id, None)
+
+        if not message_found:
+            if len(self._edit_cache) == self._edit_cache_maximum:
+                self._edit_cache.popitem(last=False)
+        return message_found
 
 
 if __name__ == '__main__':
--- cogs/owner.py
+++ cogs/owner.py
@@ -4,18 +4,21 @@ from base import custom
 
 
 # left here for easy hot-reloading
+# https://github.com/platform-discord/travis-bott/blob/master/utils/customcontext.py#L33-L79
 class Context(commands.Context):
     async def send(self, *args, **kwargs):
         is_owner = await self.bot.is_owner(self.author)
         message = self.message
 
         if is_owner:
-            cached = self.bot._edit_cache.get(self.message.id, None)
+            cached = self.bot._get_edit_cached_message(self.message.id)
 
             if cached:
                 if args:
                     kwargs["content"] = args[0]
+                await cached.clear_reactions()
                 await cached.edit(**kwargs)
+                self.bot._edit_cache[self.message.id] = cached
             else:
                 message = await super().send(*args, **kwargs)
                 self.bot._edit_cache[self.message.id] = message
